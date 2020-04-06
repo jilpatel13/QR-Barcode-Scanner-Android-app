b328068e602ae09601d9a53fb742f71306fe895b
Jil Patel <jilp131313@gmail.com>
2020-04-07 00:26:40 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/qrscanner/SplashScreen.java
@@ -0,0 +1,20 @@
+package com.example.qrscanner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+
+}
